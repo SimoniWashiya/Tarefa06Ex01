4171d1b0a11f2b0656e008e0c2352630392b2a4b
Simoni Washiya <simoni.washiya@fatec.sp.gov.br>
2024-04-17 13:37:27 -0300

    Add files via upload

---
+++ Tarefa06Ex01/src/model/Veiculo.java
@@ -0,0 +1,28 @@
+package model;
+
+public abstract class Veiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private float velocMax;
+    private int qtdRodas;
+    private Motor motor;
+    
+    public Veiculo(String placa, String marca, String modelo, String cor, float velocMax, int qtdRodas, Motor motor) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.velocMax = velocMax;
+        this.qtdRodas = qtdRodas;
+        this.motor = motor;
+    }
+    
+    public String getPlaca() {
+        return placa;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
